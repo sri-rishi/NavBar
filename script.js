5468880e8841c95e00cdd05f9964a579666ed91e
Rishi Srivastava <sririshi2612@gmail.com>
2021-06-10 12:03:43 +0530

    updated script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-const btnBars = document.querySelector("#bars-btn");
+const btnBars = document.querySelector(".bars-btn");
 const tabList = document.querySelector(".tab-list");
 
 
+    btnBars.addEventListener('click',  () => {
+        // console.log(tabList.classList)
+        // console.log(tabList.classList.contains("slide-down"))
+        if(tabList.classList.contains("slide-down")){
+            tabList.classList.remove("slide-down")
+        }else {
+           tabList.classList.add("slide-down")
+        }
+        // tabList.classList.toggle("slide-down")
+    })
 
-btnBars.addEventListener('click',  () => {
-    console.log(tabList.classList)
-    console.log(tabList.classList.contains("slide-down"))
-    if(tabList.classList.contains("slide-down")){
-        tabList.classList.remove("slide-down")
-    }else {
-       tabList.classList.add("slide-down")
-    }
-    tabList.classList.toggle("slide-down")
-})
+
